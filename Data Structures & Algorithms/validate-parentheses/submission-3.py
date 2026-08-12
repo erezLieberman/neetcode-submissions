c604c3af0c4d49ac8a2a3a42c74090538f07883a
Erez Lieberman <erez2613@gmail.com>
2026-08-12 13:41:50 +0300

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+       
+        opening = ['(', '{', '[']
+        closing = [')', '}', ']']
+    
+        stack = []
+
+        for c in s:
+            if c in opening:
+                stack.append(c)
+            else:
+                index = closing.index(c)
+                if stack and stack[len(stack)-1] == opening[index]:
+                    stack.remove(opening[index])
+                else:
+                    return False
+        
+        if stack:
+            return False
+        return True
