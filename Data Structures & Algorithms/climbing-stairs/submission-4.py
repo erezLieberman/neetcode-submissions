22d1c309380287dfa366c2e32ce69b20e560c3d5
Erez Lieberman <erez2613@gmail.com>
2026-09-14 16:29:39 +0300

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        arr = []
+        arr.insert(0, 1)
+        arr.insert(1, 1)
+        arr.insert(2, 2)
+        
+        if n > 2:
+            for i in range(3,n+1):
+                arr.insert(i,arr[i-1] + arr[i-2])
+        return arr[n]
+             
+
+        
