6fd1868a7b571473d24d7a74d6a2e78c2a8b60b8
Erez Lieberman <erez2613@gmail.com>
2026-09-15 15:10:56 +0300

    Add: house-robber - submission-9

---
+++ Data Structures & Algorithms/house-robber/submission-9.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return nums[0]
+
+        one_step_back = max(nums[0], nums[1])
+        two_steps_back = nums[0]
+
+        for i in range(2,len(nums)):
+            current = max(nums[i] + two_steps_back, one_step_back)
+            two_steps_back = one_step_back
+            one_step_back = current
+            
+        return max(two_steps_back, one_step_back)
+            
