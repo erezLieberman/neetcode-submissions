24ca93a199129dec92cc2ee18a5af4ed3f4823c3
Erez Lieberman <erez2613@gmail.com>
2026-09-15 14:56:50 +0300

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return nums[0]
+
+        memo = []
+        memo.append(nums[0])
+        memo.append(max(nums[0], nums[1]))
+
+        for i in range(2,len(nums)):
+            memo.append(max(memo[i-1], memo[i-2] + nums[i]))
+        
+        return max(memo)
+            
